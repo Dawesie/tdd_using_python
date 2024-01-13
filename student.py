a22fb31f7078d0a66cc02f09b5ffb9809ed85be3
Dawesie <marie.dawe01967@gmail.com>
2024-01-13 15:44:05 +0000

    Code for TDD using python.

--- student.py
+++ student.py
@@ -15,15 +15,26 @@ class Student:
     def full_name(self):
         return f"{self._first_name} {self._last_name}"
 
+
     @property
     def email(self):
         return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
 
 
-
-
     def alert_santa(self):
         self.naughty_list = True
 
+
+    def apply_extension(self, days):
+       self.end_date = self.end_date + timedelta(days = days)
+
+
+    def course_shedule(self):
+        response = requests.get(f"http://company.com/course-shedule/{self._last_name}/{self._first_name}")
+
+        if response.ok:
+            return response.text
+        else:
+            return "Something went wrong with the request!"
 
    
--- test_student.py
+++ test_student.py
@@ -1,25 +1,66 @@
 import unittest
 from student import Student
+from datetime import timedelta
+from unittest.mock import patch
 
 class TestStudent(unittest.TestCase):
 
-    def test_full_name(self):
-        student = Student('John', 'Doe')
+    @classmethod
+    def setUpClass(cls):
+        print('setUpClass')
 
-        self.assertEqual(student.full_name, 'John Doe')
+
+    @classmethod
+    def tearDownClass(cls):
+        print('tearDownClass')
+
+    
+    def setUp(self):
+        print('setUp')
+        self.student = Student('John', 'Doe')
+        
+
+    def tearDown(self):
+        print('tearDown')
+
+
+    def test_full_name(self):
+        print('test_full_name')
+        self.assertEqual(self.student.full_name, 'John Doe')
 
 
     def test_alert_santa(self):
-        student = Student('John', 'Doe')
-        student.alert_santa()
+        print('test_email')
+        self.student.alert_santa()
 
-        self.assertTrue(student.naughty_list)
+        self.assertTrue(self.student.naughty_list)
 
 
     def test_email(self):
-        student = Student('John', 'Doe')
-        
-        self.assertTrue(student.email, 'johndoe@email.com')
+        print('test_alert_santa')       
+        self.assertTrue(self.student.email, 'johndoe@email.com')
+
+    def test_apply_extension(self):
+        old_end_date = self.student.end_date
+        self.student.apply_extension(5)
+        self.assertEqual(self.student.end_date, old_end_date + timedelta(days=5))
+
+
+    def  test_course_schedule_sucess(self):
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = True
+            mocked_get.return_value.text = "Success"
+
+            shedule = self.student.course_shedule()
+            self.assertEqual(schedule, "Sucess")
+
+
+    def  test_course_schedule_fail(self):
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = False
+            
+            shedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Something went wrong with the request!")
 
 
 if __name__ == '__main__':
